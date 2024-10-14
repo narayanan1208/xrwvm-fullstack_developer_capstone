69c58639617fbb3ca86f808aba5286287bd88dc3
narayanan1208 <narayanan6647@gmail.com>
2024-10-14 17:16:12 -0400

    Modified files for car make and car model

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://narayanan664-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://narayanan664-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+# ALLOWED_HOSTS=['localhost','https://narayanan664-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+# CSRF_TRUSTED_ORIGINS=['https://narayanan664-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+
+ALLOWED_HOSTS=['localhost','https://narayanan664-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://narayanan664-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -140,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
-    os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    # os.path.join(BASE_DIR, 'frontend/build'),
+    # os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
